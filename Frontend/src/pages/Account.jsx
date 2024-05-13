c41eec140feb7c12ee96390c0f1d5bcee322ed88
ElgeneReyes <edreyes7@up.edu.ph>
2024-05-13 18:44:00 +0800

    feat: Initialized account page to show user details

--- Frontend/src/pages/Account.jsx
+++ Frontend/src/pages/Account.jsx
@@ -1,7 +1,40 @@
-export default function Account() {
-    return (
-        <>
-        <p> Welcome to the account page. </p>
-        </>
-    )
-}
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Account = () => {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  const fetchUserData = async () => {
+    try {
+      const email = localStorage.getItem('email');
+      const response = await axios.get(`http://localhost:4000/api/users?email=${email}`);
+      const userDataArray = response.data;
+      const user = userDataArray.find(user => user.email === email);
+      setUserData(user);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Account Information</h2>
+      {userData ? (
+        <ul>
+          <li>First Name: {userData.firstName}</li>
+          <li>Last Name: {userData.lastName}</li>
+          <li>Email: {userData.email}</li>
+          <li>User Type: {userData.userType}</li>
+        </ul>
+      ) : (
+        <div>No user data found</div>
+      )}
+    </div>
+  );
+};
+
+export default Account;
