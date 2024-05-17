0f7a4194aa1357ec5574bd3b43ba90c3d1c26a84
ElgeneReyes <edreyes7@up.edu.ph>
2024-05-18 01:41:47 +0800

    feat: Added error page for displaying generic error messsages

---
+++ Frontend/src/pages/Error.jsx
@@ -0,0 +1,10 @@
+const ErrorPage = () => {
+  return (
+    <div className="error-container">
+      <h1>Error</h1>
+      <p>Oops! Something went wrong.</p>
+    </div>
+  );
+}
+
+export default ErrorPage;
