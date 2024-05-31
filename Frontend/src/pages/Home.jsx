1876e0666c4a260157df473dfae42d1a49867c21
ElgeneReyes <edreyes7@up.edu.ph>
2024-05-31 23:28:42 +0800

    feat: Modified Home components for better div representation

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,15 +1,24 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar, faSeedling, faCow, faFish, faSquareMinus, faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { faStar, faSeedling, faCow, faFish, faSquareMinus } from '@fortawesome/free-solid-svg-icons';
 import Background from '../pictures/homepage_background.jpg';
+import { useState, useEffect } from 'react';
 import './Home.css';
 
 export default function Home() {
+    const [isActive, setIsActive] = useState(false);
+
+    useEffect(() => {
+      setIsActive(true);
+    }, []);
+
     return (
+        <div className={`fade-in-out ${isActive ? "active" : ""}`}>
         <div className="homepage-container">
             <div className="homepage-header">
                 <img className="background" src={Background} alt="Homepage Background" />
                 <div className='tagline-content'>
-                    <h2 className="tagline"> From Farm to Table, Freshness Delivered: Your Online Grocery Destination </h2>
+                    <h2 className="tagline"> From Farm to Table, Freshness Delivered: </h2>
+                    <h2 className="tagline2">Your Online Grocery Destination </h2>
                 </div>
             </div>
 
@@ -18,30 +27,26 @@ export default function Home() {
                     <div>
                         <FontAwesomeIcon icon={faStar} />
                         <li>Staple</li>
-                        <FontAwesomeIcon icon={faChevronDown} />
                     </div>
                     <div>
                         <FontAwesomeIcon icon={faSeedling} />
                         <li>Fruits and Vegetables</li>
-                        <FontAwesomeIcon icon={faChevronDown} />
                     </div>
                     <div>
                         <FontAwesomeIcon icon={faCow} />
                         <li>Livestock</li>
-                        <FontAwesomeIcon icon={faChevronDown} />
                     </div>
                     <div>
                         <FontAwesomeIcon icon={faFish} />
                         <li>Seafood</li>
-                        <FontAwesomeIcon icon={faChevronDown} />
                     </div>
                     <div>
                         <FontAwesomeIcon icon={faSquareMinus} />
                         <li>Others</li>
-                        <FontAwesomeIcon icon={faChevronDown} />
                     </div>
                 </ul>
             </div>
+        </div>
         </div>
     );
 }
