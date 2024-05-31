6fb6a59c7c25df64339f0d1169c878563449b074
ElgeneReyes <edreyes7@up.edu.ph>
2024-05-31 23:36:03 +0800

    feat: Added imageUrl in product schema

--- Backend/model/ProductSchema.js
+++ Backend/model/ProductSchema.js
@@ -4,8 +4,9 @@ const productSchema = new mongoose.Schema({
     productName: { type: String, required: true },
     productDesc: { type: String, required: true },
     productType: { type: Number, required: true },
-    productPrice: { type: Number, required: true},
+    productPrice: { type: Number, required: true },
     productQty: { type: Number, required: true },
+    imageUrl: { type: String, required: true },
 }, { collection: 'Products' });
 
 const Product = mongoose.model('Product', productSchema);
